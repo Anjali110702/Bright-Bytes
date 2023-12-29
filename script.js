34715d2881c3f7073b590b97ab71a2e861e875af
Anjali110702 <143711300+Anjali110702@users.noreply.github.com>
2023-12-29 16:56:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+      //  sticky navbar on scroll script  //
+      if (this.scrollY > 20) {
+        $(".navbar").addClass("sticky");
+      } else {
+        $(".navbar").removeClass("sticky");
+      }
+  
+      //  scroll-up button show/hide script  //
+      if (this.scrollY > 500) {
+        $(".scroll-up-btn").addClass("show");
+      } else {
+        $(".scroll-up-btn").removeClass("show");
+      }
+    });
+    $(".scroll-up-btn").click(function () {
+      $("html").animate({ scrollTop: 0 });
+      $("html").css("scrollBehavior", "auto");
+    });
+    $(".navbar .menu li a").click(function () {
+      $("html").css("scrollBehavior", "smooth");
+    });
+  });
+
+
+  
